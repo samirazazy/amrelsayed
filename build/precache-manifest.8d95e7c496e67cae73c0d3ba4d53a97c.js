0a7292d1f563b0650af5f7dbda1ece6082fec29f
samirazazy <samiralazazy@gmail.com>
2020-09-22 10:38:48 +0200

    add build

---
+++ build/precache-manifest.8d95e7c496e67cae73c0d3ba4d53a97c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35ee7da7d47f49d11ead2bd440eef6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07b1c20e3b4906483027",
+    "url": "/static/css/main.40d78683.chunk.css"
+  },
+  {
+    "revision": "4995944661065b26206e",
+    "url": "/static/js/2.10f6d4e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.10f6d4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07b1c20e3b4906483027",
+    "url": "/static/js/main.31701f7c.chunk.js"
+  },
+  {
+    "revision": "75b67e50613f7dd4cc1e",
+    "url": "/static/js/runtime-main.5a1d5d0f.js"
+  },
+  {
+    "revision": "3ca07785aa3a70531c6f8f92899642cc",
+    "url": "/static/media/MobacoTshirt.3ca07785.jpg"
+  },
+  {
+    "revision": "4ba0002dd27c1f1f824997314610a6a7",
+    "url": "/static/media/TerranovaTshirt.4ba0002d.jpg"
+  },
+  {
+    "revision": "39165b85f8bbe42158215aade5e29a3a",
+    "url": "/static/media/TerranovaTshirt2.39165b85.jpg"
+  },
+  {
+    "revision": "8b7d2a3f6828c459db0aeeeeeb22bbd5",
+    "url": "/static/media/cover.8b7d2a3f.jpg"
+  },
+  {
+    "revision": "4a682e1d151831626a189148d7d6b075",
+    "url": "/static/media/greenTshirt.4a682e1d.jpg"
+  }
+]);
